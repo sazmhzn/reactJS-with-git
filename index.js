6e2b31029ce69674f0b3cc1469973144689a6b9a
suron maharjan <suron.maharjan@gmail.com>
2022-06-13 17:57:13 +0545

    [Added]

---
+++ index.js
@@ -0,0 +1,52 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="flex-content">
+        <div className="name">Navbar</div>
+        <div className="edit">
+          <ul>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function MainComponents() {
+  return (
+    <main>
+      <h1>Main Content</h1>
+    </main>
+  );
+}
+
+
+ const element = <h1 className="header"> This is JSX </h1>
+ console.log(element);
+
+//  const page =
+//    <div>
+//      <h1 className="header">This is JSX</h1>
+//      <p>This is a paragraph</p>
+//    </div>
+
+//JSX to create a navbar
+const navBar = (
+  <nav>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+
+console.log(navBar);
+
+ //append the code in root element -->
+ ReactDOM.render(navBar, document.getElementById("root")); 
