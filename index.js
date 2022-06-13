432ff0f6181f2f46e1ff3a3e2ba1a2aecbebdb25
suron maharjan <suron.maharjan@gmail.com>
2022-06-13 18:08:34 +0545

    [Added]

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-function Navbar() {
-  return (
-    <nav>
-      <div className="flex-content">
-        <div className="name">Navbar</div>
-        <div className="edit">
-          <ul>
-            <li>home</li>
-            <li>home</li>
-            <li>home</li>
-            <li>home</li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-}
+// function Navbar() {
+//   return (
+//     <nav>
+//       <div className="flex-content">
+//         <div className="name">Navbar</div>
+//         <div className="edit">
+//           <ul>
+//             <li>home</li>
+//             <li>home</li>
+//             <li>home</li>
+//             <li>home</li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
 
-function MainComponents() {
-  return (
-    <main>
-      <h1>Main Content</h1>
-    </main>
-  );
-}
+// function MainComponents() {
+//   return (
+//     <main>
+//       <h1>Main Content</h1>
+//     </main>
+//   );
+// }
 
 
  const element = <h1 className="header"> This is JSX </h1>
  console.log(element);
 
-//  const page =
-//    <div>
-//      <h1 className="header">This is JSX</h1>
-//      <p>This is a paragraph</p>
-//    </div>
-
-//JSX to create a navbar
 const navBar = (
   <nav>
+    <h1>
+      Bos's Bistra
+    </h1>
     <ul>
       <li>Pricing</li>
       <li>About</li>
@@ -44,6 +40,7 @@ const navBar = (
     </ul>
   </nav>
 );
+
 
 
 console.log(navBar);
