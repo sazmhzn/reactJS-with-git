4e90645e55808cc686cd4d73d54086e75d0d3351
suron maharjan <suron.maharjan@gmail.com>
2022-06-16 21:50:41 +0545

    [ADDED] add header component

--- index.js
+++ index.js
@@ -2,22 +2,27 @@
 
 function Header() {
   return (
+    <header>
+      <nav>
+        <img src="./React.webp" width="40px"></img>
+      </nav>
+    </header>
+  );
+}
+
+function Page() {
+  return (
     //This is JSX
     <div>
-      <header>
-        <nav>
-          <img src="./React.webp" width="40px"></img>
-        </nav>
-      </header>
       <h1>Reasons I'm exited to learn React</h1>
-          <ol>
-            <li>It's popular library, so I'll able to fit in with the cool kids!</li>
-          </ol>
-      <footer>
-        20222 surohan development. All rights reserved.
-      </footer>
+      <ol>
+        <li>
+          It's popular library, so I'll able to fit in with the cool kids!
+        </li>
+      </ol>
+      <footer>20222 surohan development. All rights reserved.</footer>
     </div>
   );
 }
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
